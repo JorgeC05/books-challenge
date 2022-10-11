c0d92b56a65fc9e4d874d2331cc008bbdecbe012
Jorge Cortes <jorgecortesing@gmail.com>
2022-10-11 10:26:11 -0500

    Se implementa desafio 1, vista de detalle de un libro

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,44 +1,50 @@
-const bcryptjs = require('bcryptjs');
-const db = require('../database/models');
+const bcryptjs = require("bcryptjs");
+const db = require("../database/models");
 
 const mainController = {
   home: (req, res) => {
     db.Book.findAll({
-      include: [{ association: 'authors' }]
+      include: [{ association: "authors" }],
     })
       .then((books) => {
-        res.render('home', { books });
+        res.render("home", { books });
       })
       .catch((error) => console.log(error));
   },
   bookDetail: (req, res) => {
-    // Implement look for details in the database
-    res.render('bookDetail');
+    let idReference = req.params.id;
+    db.Book.findByPk(idReference, {
+      include: [ {association: 'authors'} ]
+    })
+      .then((book) => {
+        console.log(book.authors)
+        res.render("bookDetail", { book, authors: book.authors });
+    });
   },
   bookSearch: (req, res) => {
-    res.render('search', { books: [] });
+    res.render("search", { books: [] });
   },
   bookSearchResult: (req, res) => {
     // Implement search by title
-    res.render('search');
+    res.render("search");
   },
   deleteBook: (req, res) => {
     // Implement delete book
-    res.render('home');
+    res.render("home");
   },
   authors: (req, res) => {
     db.Author.findAll()
       .then((authors) => {
-        res.render('authors', { authors });
+        res.render("authors", { authors });
       })
       .catch((error) => console.log(error));
   },
   authorBooks: (req, res) => {
     // Implement books by author
-    res.render('authorBooks');
+    res.render("authorBooks");
   },
   register: (req, res) => {
-    res.render('register');
+    res.render("register");
   },
   processRegister: (req, res) => {
     db.User.create({
@@ -46,29 +52,29 @@ const mainController = {
       Email: req.body.email,
       Country: req.body.country,
       Pass: bcryptjs.hashSync(req.body.password, 10),
-      CategoryId: req.body.category
+      CategoryId: req.body.category,
     })
       .then(() => {
-        res.redirect('/');
+        res.redirect("/");
       })
       .catch((error) => console.log(error));
   },
   login: (req, res) => {
     // Implement login process
-    res.render('login');
+    res.render("login");
   },
   processLogin: (req, res) => {
     // Implement login process
-    res.render('home');
+    res.render("home");
   },
   edit: (req, res) => {
     // Implement edit book
-    res.render('editBook', {id: req.params.id})
+    res.render("editBook", { id: req.params.id });
   },
   processEdit: (req, res) => {
     // Implement edit book
-    res.render('home');
-  }
+    res.render("home");
+  },
 };
 
 module.exports = mainController;
